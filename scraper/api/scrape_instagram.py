f967870c371c9eb2a9a19472d42886129e305e1d
Eojin <raeojin@gmail.com>
2025-03-04 22:47:31 +0900

    Add script for scrolling down to load more posts an d set a limit for the number of posts to scrape

--- scraper/api/scrape_instagram.py
+++ scraper/api/scrape_instagram.py
@@ -11,10 +11,9 @@ load_dotenv()
 INSTAGRAM_USERNAME = os.getenv("INSTAGRAM_USERNAME")
 INSTAGRAM_PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
 
-# Function to get Instagram post details using Instaloader
 def get_instagram_details(url):
     INSTA_LOADER = instaloader.Instaloader()
-    shortcode = url.split("/")[-2] # Second last element of the url
+    shortcode = url.split("/")[-2]  # Extract the shortcode from the URL
     post = instaloader.Post.from_shortcode(INSTA_LOADER.context, shortcode)
 
     post_details = {
@@ -25,36 +24,49 @@ def get_instagram_details(url):
     return post_details
 
 
-# Set the path to the ChromeDriver
+# ChromeDriver
 driver_path = '/opt/homebrew/bin/chromedriver'
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 
-# Navigate to Instagram's login page
+# Login (Instagram)
 driver.get('https://www.instagram.com/accounts/login/')
 time.sleep(2)
 
-# Find the username and password input fields and send the login credentials
 username_input = driver.find_element(By.NAME, 'username')
 password_input = driver.find_element(By.NAME, 'password')
 
 username_input.send_keys(INSTAGRAM_USERNAME)  
-time.sleep(4)
+time.sleep(2)
 password_input.send_keys(INSTAGRAM_PASSWORD)
-time.sleep(4)
-password_input.send_keys(Keys.RETURN) # Enter
+time.sleep(2)
+password_input.send_keys(Keys.RETURN)  # Enter
 time.sleep(5)
 
 # Keyword
-keyword = '나이키'
+keyword = '미즈노'
 search_url = f'https://www.instagram.com/explore/tags/{keyword}/'
 
 driver.get(search_url)
-time.sleep(10)
+time.sleep(5)
 
-# Extract all post links from the page using XPath
+# Scroll down to load more posts
+scroll_limit = 3  # Scroll down 3 times
+for _ in range(scroll_limit):
+    driver.execute_script("window.scrollBy(0, 1000);") # Execute JavaScript code / scrolls 1000 pixels downward
+    time.sleep(3)  # Wait for new posts to load
+
+# Extract links
 post_links = driver.find_elements(By.XPATH, '//a[contains(@href, "/p/")]')
+
+# Set a limit for the number of posts to scrape
+max_posts = 10
+count = 0
+
 for link in post_links:
+    if count >= max_posts:  # Stop after scraping max posts
+        break
+    
     url = link.get_attribute('href')
     print(f"Post URL: {url}")
 
@@ -62,5 +74,7 @@ for link in post_links:
     print(f"Caption: {post_details['caption']}")
     print(f"User Name: {post_details['user_name']}")
     print(f"Post Date: {post_details['post_date']}")
+    
+    count += 1
 
 driver.quit()
