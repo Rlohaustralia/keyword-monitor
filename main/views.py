1ae072a655fc45205332ff6b612dea2d013cf3b6
Eojin <raeojin@gmail.com>
2025-03-25 09:58:59 +0900

    Remove error handling logic

--- main/views.py
+++ main/views.py
@@ -3,56 +3,14 @@ from django.views import generic
 from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
 from django.views import generic
 
-class aboutView(generic.TemplateView):
-    template_name = "about.html"
-    
-
-    def get(self, request, *args, **kwargs):
-        
-        if request.GET.get('error') == '404':
-            # 404 ERROR
-            return JsonResponse({'message': 'Page not found.'}, status=404)
-        elif request.GET.get('error') == '500':
-            # 500 ERROR
-            return JsonResponse({'message': 'Internal server error occurred.'}, status=500)
-        else:
-            # 200 SUCCESS
-            context = self.get_context_data(**kwargs)
-            return self.render_to_response(context)
-
 
 # Create your views here.
 
 class indexView(generic.TemplateView):
     template_name  = "index.html"
 
-    def get(self, request, *args, **kwargs):
-        
-        if request.GET.get('error') == '404':
-            # 404 ERROR
-            return JsonResponse({'message': 'Page not found.'}, status=404)
-        elif request.GET.get('error') == '500':
-            # 500 ERROR
-            return JsonResponse({'message': 'Internal server error occurred.'}, status=500)
-        else:
-            # 200 SUCCESS
-            context = self.get_context_data(**kwargs)
-            return self.render_to_response(context)
-    
-
+class aboutView(generic.TemplateView):
+    template_name = "about.html"
 
 class contactView(generic.TemplateView):
     template_name = "contact.html"
-
-    def get(self, request, *args, **kwargs):
-        
-        if request.GET.get('error') == '404':
-            # 404 ERROR
-            return JsonResponse({'message': 'Page not found.'}, status=404)
-        elif request.GET.get('error') == '500':
-            # 500 ERROR
-            return JsonResponse({'message': 'Internal server error occurred.'}, status=500)
-        else:
-            # 200 SUCCESS
-            context = self.get_context_data(**kwargs)
-            return self.render_to_response(context)
