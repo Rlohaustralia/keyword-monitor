6da6701300259b8fb7730aee991b0f130313a7eb
Eojin <raeojin@gmail.com>
2025-05-06 17:35:43 +0900

    Add view page for dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,5 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+
+@login_required
 def dashboard_view(request):
     return render(request, "dashboard_app/my_dashboard.html")
